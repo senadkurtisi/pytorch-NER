1be4cf3ba73f482e17e1558747b4a7bf12f7bf6e
senadkurtisi <senad98kurtisi@gmail.com>
2021-11-14 17:44:27 +0100

    Finalized the transformer module

--- transformer.py
+++ transformer.py
@@ -3,15 +3,41 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class PositionalEncodings(nn.Module):
+    """Attention is All You Need positional encoding layer"""
+
+    def __init__(self, seq_len, d_model, p_dropout):
+        """Initializes the layer."""
+        super(PositionalEncodings, self).__init__()
+        token_positions = torch.arange(start=0, end=seq_len).view(-1, 1)
+        dim_positions = torch.arange(start=0, end=d_model).view(1, -1)
+        angles = token_positions / (10000 ** ((2 * dim_positions) / d_model))
+
+        encodings = torch.zeros(1, seq_len, d_model)
+        encodings[0, :, ::2] = torch.cos(angles[:, ::2])
+        encodings[0, :, 1::2] = torch.sin(angles[:, 1::2])
+        encodings.requires_grad = False
+        self.register_buffer("positional_encodings", encodings)
+
+        self.dropout = nn.Dropout(p_dropout)
+
+    def forward(self, x):
+        """Performs forward pass of the module."""
+        x = x + self.positional_encodings
+        x = self.dropout(x)
+        return x
+
+
 class ScaleNorm(nn.Module):
     """Represents Scale Norm layer
-    
+
     reference: https://github.com/tnq177/transformers_without_tears
     """
     def __init__(self, scale, eps=1e-5):
         super(ScaleNorm, self).__init__()
-        self.scale = nn.Parameter(torch.tensor(scale))
-        self.eps = eps
+        scale = nn.Parameter(torch.tensor(scale))
+        self.register_buffer("eps", torch.Tensor([eps]))
+        self.register_parameter("scale", scale)
 
     def forward(self, x):
         norm = self.scale / torch.norm(x, dim=-1, keepdim=True).clamp(min=self.eps)
@@ -22,7 +48,15 @@ class TransformerEncoder(nn.Module):
     """Represents the encoder of the "Attention is All You Need" Transformer"""
 
     def __init__(self, num_layers, num_heads, d_model, ff_dim, p_dropout):
-        """Initializes the module."""
+        """Initializes the module.
+
+        Arguments:
+            num_layers (int): Number of Transformer encoder layers
+            num_heads (int): Number of self-attention heads per layer
+            d_model (int): Embedding dimension of every token
+            ff_dim (int): Number of neurons of middle layer in the feedgforward segment
+            p_dropout (float): Probability used for dropout layers
+        """
         super(TransformerEncoder, self).__init__()
         self.encoder_blocks = nn.ModuleList([
             TransformerEncoderLayer(num_heads, d_model, ff_dim, p_dropout) for _ in range(num_layers)
@@ -44,10 +78,17 @@ class TransformerEncoderLayer(nn.Module):
     """Represents a single Transformer Encoder Block."""
 
     def __init__(self, num_heads, d_model, ff_dim, p_dropout):
-        """Initializes the module."""
+        """Initializes the module.
+
+        Arguments:
+            num_heads (int): Number of self-attention heads
+            d_model (int): Embedding dimension of every token
+            ff_dim (int): Number of neurons of middle layer in the feedgforward segment
+            p_dropout (float): Probability used for dropout layers
+        """
         super(TransformerEncoderLayer, self).__init__()
         self.scale_norm_1 = ScaleNorm(d_model ** 0.5)
-        self.multi_head_attention = MultiHeadAttention(num_heads, d_model)
+        self.multi_head_attention = MultiHeadAttention(num_heads, d_model, p_dropout)
         self.dropout_1 = nn.Dropout(p_dropout)
 
         self.scale_norm_2 = ScaleNorm(d_model ** 0.5)
@@ -77,12 +118,13 @@ class TransformerEncoderLayer(nn.Module):
 class MultiHeadAttention(nn.Module):
     """Represents Multi-Head Attention Module"""
 
-    def __init__(self, num_heads, d_model):
+    def __init__(self, num_heads, d_model, p_dropout):
         """Initializes the module.
 
         Arguments:
             num_heads (int): Number of attention heads
             d_model (int): Embedding dimension of each input token
+            p_dropout (float): Probability used for dropout layers
         """
         super(MultiHeadAttention, self).__init__()
         assert (d_model % num_heads) == 0, "Embedding dimension d_model must be divisible by the number of heads!"
@@ -90,6 +132,8 @@ class MultiHeadAttention(nn.Module):
         self.num_heads = num_heads
         # Represents query, key, value matrices used for mapping the input sequence
         self.qkv_matrices = nn.ModuleList([nn.Linear(d_model, d_model) for _ in range(3)])
+
+        self.dropout = nn.Dropout(p_dropout)
         self.out_projection = nn.Linear(d_model, d_model)
 
     def self_attention(self, query, key, value, padd_mask=None):
@@ -124,6 +168,8 @@ class MultiHeadAttention(nn.Module):
             attn_scores.masked_fill_(padd_mask == torch.tensor(True), float("-inf"))
         attn_scores /= (self.d_head ** 0.5)
         attn_weights = F.softmax(attn_scores, dim=-1)
+
+        attn_weights = self.dropout(attn_weights)
 
         # Output shape: (bsz * head_dim, seq_lean, d_head)
         output = torch.bmm(attn_weights, value)
